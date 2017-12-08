aec7057f9b40c573f98ed21d7663a21e233ff028
oleivahn <oleivahn@gmail.com>
2017-12-08 03:51:06 -0500

    version 1.0

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Whopper', false);
